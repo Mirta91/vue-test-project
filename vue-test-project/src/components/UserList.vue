f7a0ed36824f3d121fcb7b02173271ab9f04ee2f
Marko Milošević <milosevic.marko86@gmail.com>
2022-02-14 10:09:33 +0100

    Dodavanje dodatnih zadataka

--- vue-test-project/src/components/UserList.vue
+++ vue-test-project/src/components/UserList.vue
@@ -2,7 +2,8 @@
   <div class="p-6">
     <h2 class="text-center font-sans text-2xl pb-10">Users Table</h2>
     <!--
-      1. prikaži podatke dobivene u varijabli users unutar tablice 
+      1. dohvati podatke sa https://jsonplaceholder.typicode.com/users kod učitavanja modula i dodaj te podatke u varijablu users
+      2. prikaži podatke dobivene u varijabli users unutar tablice 
          - tablica mora sadržavati sljedeće stupace: avatar, ime, država, opis
          - slika u avataru treba biti dimenzija 50x50px
          - tablica treba biti stilizirana (po izboru odabrati boje, razmake, veličine itd.)
@@ -14,7 +15,7 @@
        / slika.jpg    / Marko Markić   / Maroko       / Lorem ipsum dolor sit amet...
        / slika.jpg    / Ana Anić       / Angolija     / Lorem ipsum dolor sit amet...
        ...
-
+      3. Napraviti buttone za sort tablice po kolonama Ime i Država abecedno (buttoni mogu biti i izravno unutar header-a tablice za te dvije kolone)
      -->
     <table>
       <!-- ovdje napraviti tablicu -->
@@ -52,13 +53,7 @@ export default {
     return {
       users: [],
     };
-  },
-  async mounted() {
-    const { data } = await this.axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    this.users = data;
-  },
+  }
 }
 </script>
 
